83d90b1533a22df75908cbb050a85f3a2ad897f9
Luis Ardon <netoardon9@gmail.com>
2023-10-01 10:10:29 -0600

    fix: Correción para el proceso de recuperacion de cuenta

--- src/app/presentation/Forgot-password/forgot-password.component.ts
+++ src/app/presentation/Forgot-password/forgot-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, OnInit } from '@angular/core';
 import { IonicModule } from "@ionic/angular";
 import { InputComponent } from "../components/Input/input.component";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
@@ -8,21 +8,35 @@ import { patternValidator } from "../../utils/regex-validation";
 import { ForgotPasswordRepository } from "src/app/domain/repositories/forgot-password.repository";
 import { ForgotPasswordRepositoryImplementation } from "src/app/data/repositories/auth/forgot-password-implementation.repository";
 import { LoadingService } from "src/app/services/loading.service";
+import { CommonModule } from "@angular/common";
+import { VerifyCodeComponent } from "./verify-code/verify-code.component";
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
     selector: 'app-forgot-password',
     templateUrl: './forgot-password.component.html',
     standalone: true,
-    imports: [IonicModule, InputComponent, ButtonComponent, ReactiveFormsModule, RouterLink],
+    imports: 
+    [
+        IonicModule, 
+        InputComponent, 
+        ButtonComponent, 
+        ReactiveFormsModule, 
+        RouterLink,
+        CommonModule,
+        VerifyCodeComponent
+    ],
 })
-export class ForgotPasswordComponent{
+export class ForgotPasswordComponent implements OnInit {
     private userValidation = [{ regex:/^[a-zA-Z]+\.[a-zA-Z]+$/, message: 'usuario no válido' }]
+    isSendCode: boolean = false;
     _form!: FormGroup;
     constructor
     (
     private fb: FormBuilder, 
     @Inject(ForgotPasswordRepository) private forgotPasswordImplementation: ForgotPasswordRepositoryImplementation ,
-    private loadingService: LoadingService
+    private loadingService: LoadingService,
+    private alertService: AlertService,
     )
     {
         this.createForm();
@@ -32,6 +46,9 @@ export class ForgotPasswordComponent{
             username: ['', [Validators.required, patternValidator(this.userValidation)]],
         })
     }
+    ngOnInit(): void {
+        document.title = 'Recuperar contraseña';
+    }
     async NgSubmit(){
         if(!this._form.valid) return;
         try {
@@ -40,9 +57,19 @@ export class ForgotPasswordComponent{
                 username: this._form.value.username,
             }
             this.forgotPasswordImplementation.sendCodeTwoFactor(credential)
-            .subscribe(console.log);
-        } finally {
-            await this.loadingService.dismissLoading();
+            .subscribe({
+                next: (data) => {
+                    this.loadingService.dismissLoading();
+                    this.isSendCode = true;
+                    this.alertService.showAlert('Ingresa el codigo que se te envio a tu correo electrónico', 'Codigo enviado', ['Entendido'])
+                },
+                complete: () => {
+                },
+                error: (error) => {
+                    this.loadingService.dismissLoading();
+                }
+            });
+        } catch (error) {
         }
     }
 }
